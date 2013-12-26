bc8063c596c4efa35183f2100c625da45eb4796a
Mark Wallace <lotabout@gmail.com>
2013-12-27 05:27:14 +0800

    test framework for exercise 29.

---
+++ c-skeleton/tests/libex29_tests.c
@@ -0,0 +1,35 @@
+#include "minunit.h"
+
+char *test_dlopen()
+{
+    return NULL;
+}
+
+char *test_functions()
+{
+    return NULL;
+}
+
+char *test_failure()
+{
+    return NULL;
+}
+
+char *test_dlclose()
+{
+    return NULL;
+}
+
+char *all_tests()
+{
+    mu_suite_start();
+
+    mu_run_test(test_dlopen);
+    mu_run_test(test_functions);
+    mu_run_test(test_failure);
+    mu_run_test(test_dlclose);
+
+    return NULL;
+}
+
+RUN_TESTS(all_tests);
