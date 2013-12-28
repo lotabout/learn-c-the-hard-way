359bf71f8d31921b9c262fa69015181ebafce065
Mark Wallace <lotabout@gmail.com>
2013-12-28 11:01:25 +0800

    [list] Exercise 32: Double Linked Lists

---
+++ liblcthw/tests/minunit.h
@@ -0,0 +1,36 @@
+#undef NDEBUG
+#ifndef MINUNIT_H
+#define MINUNIT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <lcthw/dbg.h>
+
+
+#define mu_suite_start() char *message = NULL
+
+#define mu_assert(test, message) if (!(test)) \
+    { log_err(message); return message; }
+
+#define mu_run_test(test) debug("\n------%s", " " #test); \
+    message = (test)(); tests_run++; if (message) return message;
+
+#define RUN_TESTS(name) \
+    int main(int argc, const char *argv[]) \
+    { \
+        debug("----- RUNNING: %s", argv[0]); \
+        printf("----\nRUNNING: %s\n", argv[0]); \
+        char *result = name(); \
+        if (result != 0) { \
+            printf("FAILED: %s\n", result); \
+        } else { \
+            printf("ALL TESTS PASSED\n"); \
+        } \
+        printf("Tests run: %d\n", tests_run); \
+        exit(result != 0); \
+    }
+
+int tests_run;
+
+#endif /* end of include guard: MINUNIT_H */
